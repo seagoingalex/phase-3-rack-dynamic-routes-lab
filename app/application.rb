5382515f872af21a0d34ac4a1ac90b194ddcce37
Alex Calvert <alexanderajcalvert@gmail.com>
2021-07-09 12:41:13 -0700

    commit tree 17eae506bf4f059fcde0dfb1eec39f7100112d6b

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application
+
+    @@items = [Item.new("Apple",2),Item.new("Banana",1),Item.new("Cherries",3)]
+
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        if req.path.match(/items/)
+            item_name = req.path.split("/items/").last
+            item = @@items.find{|item| item.name == item_name}
+            if item
+                resp.write item.price
+            else 
+                resp.write "Item not found"
+                resp.status = 400
+            end
+        else 
+            resp.write "Route not found"
+            resp.status = 404
+        end
+
+        resp.finish
+    end
+end
